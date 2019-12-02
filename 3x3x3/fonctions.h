f59e8653b52b659d4eed843d79c7d16c99d0f33c
Burlesque <lbourlon@users.noreply.github.com>
2019-12-02 18:45:27 +0100

    Tout enlevé

--- 3x3x3/fonctions.h
+++
@@ -1,6 +0,0 @@
-void init(int cube[6][3][3]);
-void affiche(int cube[6][3][3]);
-void tourner(int face, int cube[6][3][3]);
-void randTour(int cube[6][3][3]);
-void melangeCube(int cube[6][3][3]);
-void copie(int cubeACopier[6][3][3], int cubeAColler[6][3][3]);
